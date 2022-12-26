5b7870656f9a62a74033a6e85b9830a8f2603575
eliott.leduc <eliott.leduc@a2display.fr>
2022-12-26 01:55:27 +0100

    initial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,173 +1,128 @@
-import * as React from "react"
+// React
+import React, { useEffect, useState } from "react";
+// Tools
+import styled from "styled-components";
+import readXlsxFile from 'read-excel-file'
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+`
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
+const ChildContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+const Table = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+const Cell = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  width: 200px
+`
 
 const IndexPage = () => {
+
+  const [file, setFile] = useState(null);
+  const [arrayTab, setArrayTab] = useState([])
+
+  const tabToSelect = ["CUISINE", "RANGEMENT", "E. M. R. T. V.", "LIBRE SERVICE"]
+
+  const handleFile = (e) => {
+    const file = e.target.files[0]
+    setFile(file)
+  }
+
+  //Use readXlsxFile to read the file and parse it in a json format tree
+  useEffect(() => {
+    if (file) {
+      let arrayIndex = []
+      let arrayTab = []
+      readXlsxFile(file, { sheet: 1 }).then((rows) => {
+        rows.map((row, index) => {
+          tabToSelect.map((tab) => {
+            if (row[0] == tab) {
+              arrayIndex.push(index)
+            }
+          })
+        })
+          console.log('arrayTab', arrayIndex)
+        if (arrayIndex.length == 4) {
+          arrayTab.push(rows.slice(arrayIndex[0], arrayIndex[1]))
+          arrayTab.push(rows.slice(arrayIndex[2], arrayIndex[3]))
+          setArrayTab(arrayTab);
+        }
+      })
+    }
+  }, [file])
+
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+    <Main> 
+      <input type="file" id="formId" onChange={(e) => handleFile(e)}/>
+      <Container>
+        <ChildContainer>
+          <Table>
+            <Row>
+              <Cell>Nom</Cell>
+              <Cell>CA</Cell>
+              <Cell>CA n-1</Cell>
+              <Cell>chépa</Cell>
+            </Row>
+            {arrayTab[0] && arrayTab[0].map((row, index) => {
+              return (
+                <Row key={index}>
+                  <Cell>{row[0]}</Cell>
+                  <Cell>{row[1]}</Cell>
+                  <Cell>{row[2]}</Cell>
+                  <Cell>{row[3]}</Cell>
+                </Row>
+              )
+            })}
+          </Table>
+        </ChildContainer>
+        <ChildContainer>
+          <Table>
+            <Row>
+              <Cell>Nom</Cell>
+              <Cell>CA</Cell>
+              <Cell>CA n-1</Cell>
+              <Cell>chépa</Cell>
+            </Row>
+            {arrayTab[1] && arrayTab[1].map((row, index) => {
+              return ( 
+                <Row key={index}>
+                  <Cell>{row[0]}</Cell>
+                  <Cell>{row[1]}</Cell>
+                  <Cell>{row[2]}</Cell>
+                  <Cell>{row[3]}</Cell>
+                </Row>
+              )
+            })}
+          </Table>
+        </ChildContainer>
+      </Container>
+    </Main>
   )
 }
 
